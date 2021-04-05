4ba92a2d1bb19c988823d0650b15645f03831f26
Lempi-sudo <andreii.lapschow1999@mail.ru>
2021-04-05 21:27:55 +0400

    tortoiseGit first commit

--- parser.py
+++ parser.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
+
 URL='https://auto.ria.com/car/hyundai/'
 HEADERS ={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.228 (Edition Yx 02)', 'accept':'*/*' }
 FILE  ='cars.csv'
